5e356cc6a84ecdb57790c3659924078a35935179
kosar Tofiq <kosar.belana@gmail.com>
2021-01-04 03:49:32 +0100

    add custom form to admin site

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+# get from settings
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin
+
+# import from our forms
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
+CustomUser = get_user_model()
+
+
+# class to show form in admin site
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['email', 'username', ]
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
