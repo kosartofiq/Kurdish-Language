7dc85ebf46959bc9bca7401904ea1cbf745af027
ksaeed <k.saeed@journaway.com>
2021-01-05 13:16:35 +0100

    delete extra test of status code

--- home/tests.py
+++ home/tests.py
@@ -13,9 +13,6 @@ class HomepageTest(SimpleTestCase):
     def test_home_page_status_code(self):
         self.assertEqual(self.response.status_code, 200)
 
-    def test_home_page_url_name(self):
-        self.assertEqual(self.response.status_code, 200)
-
     def test_home_page_template(self): 
         self.assertTemplateUsed(self.response, 'home/home.html')
 
@@ -36,9 +33,6 @@ class AboutpageTest(SimpleTestCase):
         self.response = self.client.get(url)
 
     def test_about_page_status_code(self):
-        self.assertEqual(self.response.status_code, 200)
-
-    def test_about_page_url_name(self):
         self.assertEqual(self.response.status_code, 200)
 
     def test_about_page_template(self): 
