eb0bba1260d2e5c1c28ab8f7111ae5d6e93def18
kosar Tofiq <kosar.belana@gmail.com>
2021-01-12 03:36:16 +0100

    create url file with pathes

---
+++ language/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    # path('', views.LanguageListView.as_view(), name='language'),
+    # path('<int:pk>/', views.LanguageDetailView.as_view(), name='language-detail'),
+    # path('<int:pk>/update', views.LanguageUpdateView.as_view(), name='language-update'),
+    # path('new/', views.LanguageCreateView.as_view(), name='language-create'),
+
+    # path('<int:pk>/dialect/', views.dialectListView, name='dialect'),
+    # path('<int:pk1>/dialect/<int:pk2>/detail', views.dialectDetailView, name='dialect-detail'),
+    # path('<int:pk1>/dialect/<int:pk2>/update', views.dialectUpdateView, name='dialect-update'),
+    # path('<int:pk1>/dialect/<int:pk2>/new/', views.dialectCreateView, name='dialect-create'),
+
+]
