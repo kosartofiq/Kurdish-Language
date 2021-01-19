15c0f0c433a95b1d7fbc974c67c4c7a37f3508de
kosar Tofiq <kosar.belana@gmail.com>
2021-01-19 02:40:06 +0100

    better check for language test

--- language/tests.py
+++ language/tests.py
@@ -16,37 +16,39 @@ class LanguaeTests(TestCase):
 
         self.language = Language.objects.create(
             creator=self.user,
-            name='English',
-            native_name='Englizi',
-            iso_639_1='en',
-            iso_639_2='eng',
-            description='English is a language.'
+            name='a language',
+            native_name='a native name language',
+            iso_639_1='al',
+            iso_639_2='alg',
+            description='a language is a language.'
         )
 
     # model testing
     def test_language_model(self):
-        self.assertEqual(f'{self.language.name}', 'English')
-        self.assertEqual(f'{self.language.native_name}', 'Englizi')
-        self.assertEqual(f'{self.language.iso_639_1}', 'en')
-        self.assertEqual(f'{self.language.iso_639_2}', 'eng')
-        self.assertEqual(f'{self.language.description}', 'English is a language.')
+        self.assertEqual(f'{self.language.name}', 'a language')
+        self.assertEqual(f'{self.language.native_name}', 'a native name language')
+        self.assertEqual(f'{self.language.iso_639_1}', 'al')
+        self.assertEqual(f'{self.language.iso_639_2}', 'alg')
+        self.assertEqual(f'{self.language.description}', 'a language is a language.')
 
     # list view
     def test_language_list_view(self):
         response = self.client.get(reverse('language-list'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'English')
-        self.assertContains(response, 'Englizi')
+        self.assertContains(response, 'a language')
+        self.assertContains(response, 'a native name language')
         self.assertTemplateUsed(response, 'language/language_list.html')
-
+    
+    # detail view
     def test_language_detail_view(self):
         response = self.client.get(self.language.get_absolute_url())
         no_response = self.client.get('/language/9876543210/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
-        self.assertContains(response, 'Kurdish')
-        self.assertContains(response, 'ku')
-        self.assertContains(response, 'kur')
-        self.assertContains(response, 'kurdish is a language.')
+        self.assertContains(response, 'a language')
+        self.assertContains(response, 'a native name language')
+        self.assertContains(response, 'al')
+        self.assertContains(response, 'alg')
+        self.assertContains(response, 'a language is a language.')
         self.assertTemplateUsed(response, 'languages/language_detail.html')
 
