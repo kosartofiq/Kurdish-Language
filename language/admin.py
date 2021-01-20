3d3ed2cd96a08f91640b0c37693df430466bfcdf
ksaeed <k.saeed@journaway.com>
2021-01-20 17:21:47 +0100

    register language models in admin py

--- language/admin.py
+++ language/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Language
+from .models import Language, LanguageHistory, Dialect, DialectHistory
 
 # Register your models here.
 admin.site.register(Language)
+admin.site.register(LanguageHistory)
+admin.site.register(Dialect)
+admin.site.register(DialectHistory)
