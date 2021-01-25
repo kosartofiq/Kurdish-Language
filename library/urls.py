f1182e7fb0ec856198ac4d2ae204531d975673fb
ksaeed <k.saeed@journaway.com>
2021-01-25 01:42:36 +0100

    add view and url to job, location part

--- library/urls.py
+++ library/urls.py
@@ -2,27 +2,31 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # library
     path('', views.library, name='library'),
     #path('book/new/', views.BookCreateView.as_view(), name='book-create'),
 
-
-
+    # genre
     path('genre/', views.GenreListView.as_view(), name='genre-list'),
     path('genre/<int:pk>/', views.GenreDetailView.as_view(), name='genre-detail'),
     path('genre/<int:pk>/histories/', views.genre_histories, name='genre-histories'),
     path('genre/new/', views.GenreCreateView.as_view(), name='genre-create'),
     path('genre/<int:pk>/update', views.GenreUpdateView.as_view(), name='genre-update'),
 
+    # job
+    path('job/', views.JobListView.as_view(), name='job-list'),
+    path('job/<int:pk>/', views.JobDetailView.as_view(), name='job-detail'),
+    path('job/<int:pk>/histories/', views.job_histories, name='job-histories'),
+    path('job/new/', views.JobCreateView.as_view(), name='job-create'),
+    path('job/<int:pk>/update', views.JobUpdateView.as_view(), name='job-update'),
 
-    #path('job/', views.JobListView.as_view(), name='job'),
-    #path('job/<int:pk>/', views.JobDetailView.as_view(), name='job-detail'),
-    #path('job/<int:pk>/update', views.JobUpdateView.as_view(), name='job-update'),
-    #path('job/new/', views.JobCreateView.as_view(), name='job-create'),
+    # location
+    path('location/', views.LocationListView.as_view(), name='location-list'),
+    path('location/<int:pk>/', views.LocationDetailView.as_view(), name='location-detail'),
+    path('location/<int:pk>/histories/', views.location_histories, name='location-histories'),
+    path('location/new/', views.LocationCreateView.as_view(), name='location-create'),
+    path('location/<int:pk>/update', views.LocationUpdateView.as_view(), name='location-update'),
 
-    #path('location/', views.LocationListView.as_view(), name='location'),
-    #path('location/<int:pk>/', views.LocationDetailView.as_view(), name='location-detail'),
-    #path('location/<int:pk>/update', views.LocationUpdateView.as_view(), name='location-update'),
-    #path('location/new/', views.LocationCreateView.as_view(), name='location-create'),
 
     #path('publisher/', views.PublisherListView.as_view(), name='publisher'),
     #path('publisher/<int:pk>/', views.PublisherDetailView.as_view(), name='publisher-detail'),
--- library/views.py
+++ library/views.py
@@ -64,6 +64,105 @@ class GenreUpdateView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
         return super().form_valid(form)
 
 
+# #########################
+# Job
+# #########################
+class JobListView(ListView):
+    model = Job
+    context_object_name = 'jobs'
+    ordering = ['name']
+
+
+class JobDetailView(DetailView):
+    model = Job
+
+
+def job_histories(request, pk):
+    job = Job.objects.get(pk=pk)
+    histories = JobHistory.objects.filter(job=job).order_by('-timestamp')
+    rendered_histories_html = render_to_string('library/job_histories.html', {'histories': histories})
+    return_json_data = {
+        'histories_html': rendered_histories_html,
+    }
+    return JsonResponse(return_json_data)
+
+
+class JobCreateView(SuccessMessageMixin, LoginRequiredMixin, CreateView):
+    model = Job
+    fields = ['name', 'description']
+    success_message = _(f'New job was created successfully.')
+
+    def form_valid(self, form):
+        form.instance.creator = self.request.user
+        return super().form_valid(form)
+
+
+class JobUpdateView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
+    model = Job
+    fields = ['name', 'description']
+    success_message = _(f"Information was updated successfully.")
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['update'] = True
+        return context
+
+    def form_valid(self, form):
+        form.instance.creator = self.request.user
+        return super().form_valid(form)
+
+
+# #########################
+# Location
+# #########################
+class LocationListView(ListView):
+    model = Location
+    context_object_name = 'locations'
+    ordering = ['name']
+
+
+class LocationDetailView(DetailView):
+    model = Location
+
+
+def location_histories(request, pk):
+    location = Location.objects.get(pk=pk)
+    histories = LocationHistory.objects.filter(location=location).order_by('-timestamp')
+    rendered_histories_html = render_to_string('library/location_histories.html', {'histories': histories})
+    return_json_data = {
+        'histories_html': rendered_histories_html,
+    }
+    return JsonResponse(return_json_data)
+
+
+class LocationCreateView(SuccessMessageMixin, LoginRequiredMixin, CreateView):
+    model = Location
+    fields = ['name', 'description']
+    success_message = _(f'New location was created successfully.')
+
+    def form_valid(self, form):
+        form.instance.creator = self.request.user
+        return super().form_valid(form)
+
+
+class LocationUpdateView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
+    model = Location
+    fields = ['name', 'description']
+    success_message = _(f"Information was updated successfully.")
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['update'] = True
+        return context
+
+    def form_valid(self, form):
+        form.instance.creator = self.request.user
+        return super().form_valid(form)
+
 
 
 
